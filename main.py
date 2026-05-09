c34f43125697020a67bc7de200c0e6e5d2c0c289
Ahmad Hassan <a91779125@gmail.com>
2026-05-10 00:20:04 +0500

    Added dummy website for Render free tier

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from keep_alive import keep_alive
 from bot.discord_client import bot
 
 # .env file se hidden variables load karna
@@ -12,6 +13,7 @@ if __name__ == '__main__':
     print("Bot ko start kiya ja raha hai...")
     # Bot ko run karna
     if TOKEN:
+        keep_alive()
         bot.run(TOKEN)
     else:
         print("Error: .env file mein DISCORD_TOKEN nahi mila!")
